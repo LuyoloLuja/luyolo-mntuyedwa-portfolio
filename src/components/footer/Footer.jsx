a42c286838bce95cab32adab9ec11913a5deed2c
LuyoloLuja <luyolom19@gmail.com>
2022-03-03 15:22:03 +0200

    completed my footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import {AiFillLinkedin} from 'react-icons/ai'
+import {BsFacebook} from 'react-icons/bs'
+import {AiOutlineTwitter} from 'react-icons/ai'
+
 import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>luyoloMntuyedwa</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        {/* <h4>Add me on:</h4> */}
+        <a href="https://www.linkedin.com/in/luyolo-mntuyedwa-48b593201/"><AiFillLinkedin/></a>
+        <a href="https://www.facebook.com/luyololuja.mntuyedwa/"><BsFacebook/></a>
+        <a href="https://twitter.com/iamLuyolo_"><AiOutlineTwitter/></a>
+      </div>
+
+      <div className="footer__copyright">
+          <small>&copy; Luyolo Mntuyedwa. All rights reserved. 2022</small>
+        </div>
+    </footer>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -7,7 +7,6 @@ import {FaUserAlt} from 'react-icons/fa'
 import {MdMiscellaneousServices} from 'react-icons/md'
 import {GoMail} from 'react-icons/go'
 import {FaAward} from 'react-icons/fa'
-import {GiSkills} from 'react-icons/gi'
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#');
@@ -18,7 +17,6 @@ const Nav = () => {
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserAlt/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FaAward/></a>
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''} ><MdMiscellaneousServices/></a>
-      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><GiSkills/></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><GoMail/></a>
     </nav>
   )
