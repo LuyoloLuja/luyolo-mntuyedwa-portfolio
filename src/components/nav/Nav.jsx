878a331e45de30850dc96f38c9b02260480483ae
LuyoloLuja <luyolom19@gmail.com>
2022-03-10 11:57:45 +0200

    added certification section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -7,6 +7,7 @@ import {FaUserAlt} from 'react-icons/fa'
 import {MdMiscellaneousServices} from 'react-icons/md'
 import {GoMail} from 'react-icons/go'
 import {FaAward} from 'react-icons/fa'
+import {GiSkills} from 'react-icons/gi'
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#');
@@ -15,7 +16,7 @@ const Nav = () => {
     <nav>
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><FaHome/></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserAlt/></a>
-      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FaAward/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><GiSkills/></a>
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''} ><MdMiscellaneousServices/></a>
       <a href="#certicications" onClick={() => setActiveNav('#certicications')} className={activeNav === '#certicications' ? 'active' : ''} ><FaAward/></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><GoMail/></a>
