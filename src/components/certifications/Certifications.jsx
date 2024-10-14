110c0329113b006303fc8fc1da455c3c9d0dc753
LuyoloLuja <luyolo.mntuyedwa97@gmail.com>
2024-10-14 16:44:42 +0200

    added new certification

--- src/components/certifications/Certifications.jsx
+++ src/components/certifications/Certifications.jsx
@@ -9,6 +9,7 @@ import 'swiper/css/pagination';
 
 import './Certifications.css';
 import IMG from '../../assets/codex.jpg';
+import IMG0 from '../../assets/freecodecamp-csharp-foundational.png';
 import IMG1 from '../../assets/simplilearn.jpg';
 import IMG2 from '../../assets/Learning JDBC.jpg';
 import IMG3 from '../../assets/freecodecamp.png';
@@ -17,6 +18,9 @@ const certificates = [
   // {
   //   avatar: IMG
   // },
+  {
+    avatar: IMG0
+  },
   {
     avatar: IMG1,
   },
